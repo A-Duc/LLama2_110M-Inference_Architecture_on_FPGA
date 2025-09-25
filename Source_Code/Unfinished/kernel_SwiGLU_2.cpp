cbca76a2121bbbe0e9d711ac8c0d2e9fd8b34e8c
A-Duc <23520304@gm.uit.edu.vn>
2025-09-25 08:26:19 +0700

    Change folder's name: Referrence_Code -> Reference_Code

--- Source_Code/Unfinished/kernel_SwiGLU_2.cpp
+++ Source_Code/Unfinished/kernel_SwiGLU_2.cpp
@@ -6,26 +6,59 @@
 #include "tensor.hpp"
 #include "kernel_SwiGLU.hpp"
 
+#define MAX_TENSOR_SIZE 3072
+
 using namespace std;
 
-static void load_tensor_1d(hls::stream<float> &in_stream, float* local_vec, int size){
+static void push_tensor1d(hls::stream<float> &in_stream, float* tensor, int size){
     for (int i = 0; i < size; i++)
 #pragma HLS PIPELINE II=1
-        in_stream << local_vec[i];
+        in_stream.write(tensor[i]);
 }
 
-static void load_tensor_2d(hls::stream<float> &in_stream, float* local_vec, int col, int row){
-    for (int i = 0; i < col; i++)
-        for (int j = 0; j < row; j++){
+static void push_tensor2d_bycol(hls::stream<float> &in_stream, float* tensor, int col, int row){
+    for (int c = 0; c < col; c++)
+        for (int r = 0; r < row; r++){
 #pragma HLS PIPELINE II=1
-            in_stream << local_vec[i * row + j];
+            in_stream.write(tensor[r * col + c]);
         }
 }
 
-static void store_tensor_1d(hls::stream<float> &out_stream, float* local_vec, int size){
+static void Multiply_VecMat(int col, int row, int size,
+                            hls::stream<float> &vec_stream, 
+                            hls::stream<float> &mat_stream,
+                            hls::stream<float> &res_stream){
+
+    if (size != row || size > MAX_TENSOR_SIZE || col > MAX_TENSOR_SIZE) return;
+                    
+    float vec[MAX_TENSOR_SIZE];
     for (int i = 0; i < size; i++)
 #pragma HLS PIPELINE II=1
-        local_vec[i] >> out_stream;
+        vec[i] = vec_stream.read();
+    
+    for (int c = 0; c < col; c++){
+        float res = 0.0;
+        float mat_element = 0.0;
+
+        for (int r = 0; r < row; r++){
+#pragma HLS PIPELINE II=1
+            mat_element = mat_stream.read();
+            res += vec[r] * mat_element;
+        }
+        res_stream.write(res);
+    }
+        
 }
 
-static void Sigmoid
+static Plus_Vec(int size
+                hls::stream<float> &A
+                hls::stream<float> &B
+                hls::stream<float> &Sum){
+    
+    float s = 0;
+    for (int i = 0; i++; i < size){
+        s = A.read() + B.read();
+        Sum.write(s);
+    }
+}
+
